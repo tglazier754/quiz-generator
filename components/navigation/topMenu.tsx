329ea37bed38607f5e4accf683a4cafb9b98e2f8
Taylor <taylor_754@hotmail.com>
2024-10-25 15:45:22 -0400

    Working on top nav

--- components/navigation/topMenu.tsx
+++ components/navigation/topMenu.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
-import { Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Spacer, Square } from "@chakra-ui/react";
+import { Circle, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Spacer, Square } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import UserMenu from "./userMenu";
 
@@ -8,21 +8,17 @@ export const TopMenu = async () => {
     const supabaseConnection = createClient();
     const { data: userData, error: userError } = await supabaseConnection.auth.getUser();
 
-    console.log(userData);
-
-    const logoutAction = () => {
-        console.log("logout");
-    }
-
     return (
-        <div className="w-screen top-0 h-10 bg-white">
+        <div className="w-screen top-0 h-12 p-0 bg-white">
 
-            <Flex>
-                <Square bg="red">
+            <Flex alignItems="center" className="min-h-12 pr-4 pl-4">
+                <Circle bg="red" color="white">
                     TG
-                </Square>
+                </Circle>
                 <Spacer />
-                <UserMenu />
+                <div className="h-full">
+                    <UserMenu />
+                </div>
             </Flex>
         </div>
     )
